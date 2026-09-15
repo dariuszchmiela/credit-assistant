4cb3fca3146535cbe946f8f351250c4edf7c92fd
Dariusz Chmiela <dariusz.chmiela@o2.pl>
2026-09-15 05:23:54 +0200

    Initial commit

---
+++ src/main/java/pl/dch/creditassistant/CreditAssistantApplication.java
@@ -0,0 +1,13 @@
+package pl.dch.creditassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/dch/creditassistant/CreditAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.dch.creditassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
